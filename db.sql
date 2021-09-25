18de3c3a873b126a1a71b996bf70e137d4ff8b88
Yas <irfanyasiras@gmail.com>
2021-09-25 20:20:23 +0700

    fix: db syntax

--- db.sql
+++ db.sql
@@ -1,20 +1,20 @@
-CREATE TABLE sql6439061.t_user (
+CREATE TABLE db_golang_yasir.t_user (
     id INT auto_increment primary key,
     username VARCHAR(255) not null UNIQUE,
-    'password' VARCHAR(255) not null,
+    `password` VARCHAR(255) not null,
     full_name VARCHAR(255) not null,
     birth_date VARCHAR(255) not null,
     image_url VARCHAR(255) not null,
     phone VARCHAR(255) not null,
     email VARCHAR(255) not null,
-    'address' VARCHAR(255) not null,
+    `address` VARCHAR(255) not null,
     role_id INT not null,
     created_at DATETIME not null,
     updated_at DATETIME not null
 );
 
 
-CREATE TABLE sql6439061.t_role (
+CREATE TABLE db_golang_yasir.t_role (
     id INT auto_increment primary key,
     title VARCHAR(255) not null,
     detail VARCHAR(255) not null,
@@ -22,24 +22,24 @@ CREATE TABLE sql6439061.t_role (
     updated_at DATETIME not null
 );
 
-CREATE TABLE sql6439061.t_shop (
+CREATE TABLE db_golang_yasir.t_shop (
     id INT auto_increment primary key,
     tag VARCHAR(255) not null UNIQUE,
-    'name' VARCHAR(255) not null,
+    `name` VARCHAR(255) not null,
     detail VARCHAR(255) not null,
     image_url VARCHAR(255) not null,
     phone VARCHAR(255) not null,
     email VARCHAR(255) not null,
-    'address' VARCHAR(255) not null,
+    `address` VARCHAR(255) not null,
     is_active INT not null,
-    'user_id' INT not null,
+    `user_id` INT not null,
     created_at DATETIME not null,
     updated_at DATETIME not null
 );
 
-CREATE TABLE sql6439061.t_product (
+CREATE TABLE db_golang_yasir.t_product (
     id INT auto_increment primary key,
-    'name' VARCHAR(255) not null,
+    `name` VARCHAR(255) not null,
     detail VARCHAR(255) not null,
     category VARCHAR(255) not null,
     image_url VARCHAR(255) not null,
@@ -50,30 +50,30 @@ CREATE TABLE sql6439061.t_product (
     updated_at DATETIME not null
 );
 
-CREATE TABLE sql6439061.t_review (
+CREATE TABLE db_golang_yasir.t_review (
     id INT auto_increment primary key,
     note VARCHAR(255) not null,
     response VARCHAR(255),
     media_url VARCHAR(255),
     rating INT not null,
-    'user_id' INT not null,
+    `user_id` INT not null,
     product_id INT not null,
     created_at DATETIME not null,
     updated_at DATETIME not null
 );
 
-CREATE TABLE sql6439061.t_session (
+CREATE TABLE db_golang_yasir.t_session (
     id INT auto_increment primary key,
     courier VARCHAR(255),
     note VARCHAR(255),
     delivery_cost DECIMAL not null,
-    'user_id' INT not null,
+    `user_id` INT not null,
     status_id INT not null,
     created_at DATETIME not null,
     updated_at DATETIME not null
 );
 
-CREATE TABLE sql6439061.t_status (
+CREATE TABLE db_golang_yasir.t_status (
     id INT auto_increment primary key,
     title VARCHAR(255) not null,
     detail VARCHAR(255) not null,
@@ -81,24 +81,24 @@ CREATE TABLE sql6439061.t_status (
     updated_at DATETIME not null
 );
 
-CREATE TABLE sql6439061.t_cart (
+CREATE TABLE db_golang_yasir.t_cart (
     id INT auto_increment primary key,
     product_name VARCHAR(255) not null,
     note VARCHAR(255),
     product_price DECIMAL not null,
     product_price_mod DECIMAL not null,
     amount INT not null,
-    'session_id' INT not null,
+    `session_id` INT not null,
     created_at DATETIME not null,
     updated_at DATETIME not null
 );
 
 
-INSERT INTO sql6439061.t_user (username, 'password', full_name, birth_date, image_url, phone, email, 'address', 'role_id', created_at, updated_at) values('master', '123456', 'Irfan Yas', '1995-08-02', 'https://ui-avatars.com/api/?name=Irfan%20Yas&background=random', '085155112341', 'irfanyasiras@gmail.com', 'Kota Bandung', 1, NOW(), NOW());
-INSERT INTO sql6439061.t_role (title, detail, created_at, updated_at) values('ADMIN', 'Role for developer', NOW(), NOW());
-INSERT INTO sql6439061.t_role (title, detail, created_at, updated_at) values('USER', 'Role for customers and sellers', NOW(), NOW());
-INSERT INTO sql6439061.t_status (title, detail, created_at, updated_at) values('CART', 'Masih dalam keranjang pembeli', NOW(), NOW());
-INSERT INTO sql6439061.t_status (title, detail, created_at, updated_at) values('PREPARING', 'Dalam persiapan untuk dikirim oleh penjual', NOW(), NOW());
-INSERT INTO sql6439061.t_status (title, detail, created_at, updated_at) values('ON DELIVERY', 'Dalam pengiriman', NOW(), NOW());
-INSERT INTO sql6439061.t_status (title, detail, created_at, updated_at) values('FINISHED', 'Berhasil dikirim', NOW(), NOW());
-INSERT INTO sql6439061.t_status (title, detail, created_at, updated_at) values('CANCELED', 'Dibatalkan', NOW(), NOW());
+INSERT INTO db_golang_yasir.t_user (username, `password`, full_name, birth_date, image_url, phone, email, `address`, `role_id`, created_at, updated_at) values('master', '123456', 'Irfan Yas', '1995-08-02', 'https://ui-avatars.com/api/?name=Irfan%20Yas&background=random', '085155112341', 'irfanyasiras@gmail.com', 'Kota Bandung', 1, NOW(), NOW());
+INSERT INTO db_golang_yasir.t_role (title, detail, created_at, updated_at) values('ADMIN', 'Role for developer', NOW(), NOW());
+INSERT INTO db_golang_yasir.t_role (title, detail, created_at, updated_at) values('USER', 'Role for customers and sellers', NOW(), NOW());
+INSERT INTO db_golang_yasir.t_status (title, detail, created_at, updated_at) values('CART', 'Masih dalam keranjang pembeli', NOW(), NOW());
+INSERT INTO db_golang_yasir.t_status (title, detail, created_at, updated_at) values('PREPARING', 'Dalam persiapan untuk dikirim oleh penjual', NOW(), NOW());
+INSERT INTO db_golang_yasir.t_status (title, detail, created_at, updated_at) values('ON DELIVERY', 'Dalam pengiriman', NOW(), NOW());
+INSERT INTO db_golang_yasir.t_status (title, detail, created_at, updated_at) values('FINISHED', 'Berhasil dikirim', NOW(), NOW());
+INSERT INTO db_golang_yasir.t_status (title, detail, created_at, updated_at) values('CANCELED', 'Dibatalkan', NOW(), NOW());
